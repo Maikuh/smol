a23ce7c7c8f28b834052bb4571e52587783612f2
Miguel Araujo <miguelant.araujo@gmail.com>
2020-10-04 10:49:26 -0400

    Changed server start console.log. Added README.

--- index.js
+++ index.js
@@ -1,104 +1,109 @@
-const Koa = require('koa');
-const Router = require('@koa/router');
-const ratelimit = require('koa-ratelimit');
-const { createReadStream } = require('fs')
-const { nanoid } = require('nanoid')
-const yup = require('yup');
+const Koa = require("koa");
+const Router = require("@koa/router");
+const ratelimit = require("koa-ratelimit");
+const { createReadStream } = require("fs");
+const { nanoid } = require("nanoid");
+const yup = require("yup");
 
-if (process.env.NODE_ENV !== "production")
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const db = require('monk')(process.env.MONGO_URI || 'localhost/smol')
+const db = require("monk")(process.env.MONGO_URI || "localhost/smol");
 
-const smols = db.get('smols')
-smols.createIndex('slug', { unique: true })
-smols.createIndex('url', { unique: true })
+const smols = db.get("smols");
+smols.createIndex("slug", { unique: true });
+smols.createIndex("url", { unique: true });
 
 const app = new Koa();
 const router = new Router();
 const memoryDb = new Map();
 
 const schema = yup.object().shape({
-    slug: yup.string().trim().matches(/^[\w\-]+$/i),
+    slug: yup
+        .string()
+        .trim()
+        .matches(/^[\w\-]+$/i),
     url: yup.string().trim().url().required(),
 });
 
-app.use(require('koa-logger')())
-app.use(require('koa-bodyparser')());
-app.use(require('koa-static')("public"));
-app.use(require('koa-helmet')())
-app.use(ratelimit({
-    driver: 'memory',
-    db: memoryDb,
-    duration: 60 * 1000,
-    errorMessage: { error: "Rate Limit", message: 'Slow down boi' },
-    id: (ctx) => ctx.ip,
-    headers: {
-        remaining: 'Rate-Limit-Remaining',
-        reset: 'Rate-Limit-Reset',
-        total: 'Rate-Limit-Total'
-    },
-    max: 10,
-    disableHeader: false
-}));
+app.use(require("koa-logger")());
+app.use(require("koa-bodyparser")());
+app.use(require("koa-static")("public"));
+app.use(require("koa-helmet")());
+app.use(
+    ratelimit({
+        driver: "memory",
+        db: memoryDb,
+        duration: 60 * 1000,
+        errorMessage: { error: "Rate Limit", message: "Slow down boi" },
+        id: (ctx) => ctx.ip,
+        headers: {
+            remaining: "Rate-Limit-Remaining",
+            reset: "Rate-Limit-Reset",
+            total: "Rate-Limit-Total",
+        },
+        max: 10,
+        disableHeader: false,
+    })
+);
 
-router.get('/smols', async ctx => {
+router.get("/smols", async (ctx) => {
     ctx.body = {
-        smols: await smols.find()
-    }
+        smols: await smols.find(),
+    };
 });
 
-router.get("/:id", async ctx => {
-    const { id } = ctx.params
+router.get("/:id", async (ctx) => {
+    const { id } = ctx.params;
 
-    const smol = await smols.findOne({ slug: id })
+    const smol = await smols.findOne({ slug: id });
 
     if (!smol) {
-        ctx.type = "text/html"
-        ctx.status = 404
-        return ctx.body = createReadStream("public/404.html")
+        ctx.type = "text/html";
+        ctx.status = 404;
+        return (ctx.body = createReadStream("public/404.html"));
     }
 
-    ctx.redirect(smol.url)
-})
+    ctx.redirect(smol.url);
+});
 
-router.post("/new", async ctx => {
-    const { url } = ctx.request.body
+router.post("/new", async (ctx) => {
+    const { url } = ctx.request.body;
 
     try {
-        await schema.validate({ url })
+        await schema.validate({ url });
     } catch (error) {
-        ctx.status = 400
-        return ctx.body = {
+        ctx.status = 400;
+        return (ctx.body = {
             error: "Validation Error",
-            message: error.errors
-        }
+            message: error.errors,
+        });
     }
 
-    const existingSmol = await smols.findOne({ url }, { projection: { slug: 1 } })
+    const existingSmol = await smols.findOne(
+        { url },
+        { projection: { slug: 1 } }
+    );
 
-    if (existingSmol)
-        return ctx.body = existingSmol
+    if (existingSmol) return (ctx.body = existingSmol);
 
-    const smol = await smols.insert({ url, slug: nanoid(5) })
+    const smol = await smols.insert({ url, slug: nanoid(5) });
 
     ctx.body = {
         message: "Smol created successfully!",
-        slug: smol.slug
-    }
-})
+        slug: smol.slug,
+    };
+});
 
-app.use(router.routes())
-    .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(ctx => {
-    ctx.type = "text/html"
-    ctx.status = 404
-    return ctx.body = createReadStream("public/404.html")
-})
+app.use((ctx) => {
+    ctx.type = "text/html";
+    ctx.status = 404;
+    return (ctx.body = createReadStream("public/404.html"));
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Successfully started serversssssssssssssssssss at http://localhost:${PORT}`)
+    console.log(`Successfully started server at http://localhost:${PORT}`);
 });
